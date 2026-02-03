722071e4b28bdcce3744fc14127bfcf799b5891b
ritvars <ritvarszvejnieks@protonmail.com>
2026-02-03 21:28:10 +0200

    style: move internal ui helper functions.

--- src/ui.rs
+++ src/ui.rs
@@ -30,60 +30,6 @@ pub const STATE_OFFSET_LEFT: u8 = 10;
 pub const PREVIEW_OFFSET_LEFT: u8 = STATE_OFFSET_LEFT + UI_SCALE;
 pub const PREVIEW_OFFEST_TOP: u8 = 170; // from 0 to 200
 
-fn get_state_btn(state: UiState) -> insim::insim::Btn {
-    let text = match state {
-        UiState::Idle => "^2•",
-        UiState::Recording => "^1•",
-        UiState::Processing => "^3•",
-        UiState::Stopped => "",
-    };
-
-    insim::insim::Btn{
-        t: PREVIEW_OFFEST_TOP,
-        w: UI_SCALE,
-        h: UI_SCALE,
-        l: STATE_OFFSET_LEFT,
-        reqi: insim::identifiers::RequestId::from(1),
-        ucid: insim::identifiers::ConnectionId::LOCAL,
-        clickid: insim::identifiers::ClickId::from(STATE_ID),
-        inst: insim::insim::BtnInst::default(),
-        bstyle: insim::insim::BtnStyle{
-            colour: insim::insim::BtnStyleColour::NotEditable,
-            flags: insim::insim::BtnStyleFlags::LIGHT,
-        },
-        typein: None,
-        caption: None,
-        text: text.to_string(),
-    }
-}
-
-// depending on charaters used, width may vary
-fn msg_to_btn_width(message: String) -> u8 {
-    let len = message.len();
-    let width = (len as f32 * 0.75).ceil() as u8 + UI_SCALE;
-    width.clamp(5, 200)
-}
-
-fn get_message_preview_btn(message: String) -> insim::insim::Btn {
-    insim::insim::Btn{
-        t: PREVIEW_OFFEST_TOP,
-        w: msg_to_btn_width(message.clone()),
-        h: UI_SCALE,
-        l: PREVIEW_OFFSET_LEFT,
-        reqi: insim::identifiers::RequestId::from(1),
-        ucid: insim::identifiers::ConnectionId::LOCAL,
-        clickid: insim::identifiers::ClickId::from(PREVIEW_ID),
-        inst: insim::insim::BtnInst::default(),
-        bstyle: insim::insim::BtnStyle{
-            colour: insim::insim::BtnStyleColour::NotEditable,
-            flags: insim::insim::BtnStyleFlags::LIGHT | insim::insim::BtnStyleFlags::LEFT,
-        },
-        typein: None,
-        caption: None,
-        text: format!("^3{}", message),
-    }
-}
-
 pub struct UiContext {
     pub state: UiState,
     pub message: String,
@@ -236,3 +182,58 @@ impl UiContext {
         }
     }
 }
+
+fn get_state_btn(state: UiState) -> insim::insim::Btn {
+    let text = match state {
+        UiState::Idle => "^2•",
+        UiState::Recording => "^1•",
+        UiState::Processing => "^3•",
+        UiState::Stopped => "",
+    };
+
+    insim::insim::Btn{
+        t: PREVIEW_OFFEST_TOP,
+        w: UI_SCALE,
+        h: UI_SCALE,
+        l: STATE_OFFSET_LEFT,
+        reqi: insim::identifiers::RequestId::from(1),
+        ucid: insim::identifiers::ConnectionId::LOCAL,
+        clickid: insim::identifiers::ClickId::from(STATE_ID),
+        inst: insim::insim::BtnInst::default(),
+        bstyle: insim::insim::BtnStyle{
+            colour: insim::insim::BtnStyleColour::NotEditable,
+            flags: insim::insim::BtnStyleFlags::LIGHT,
+        },
+        typein: None,
+        caption: None,
+        text: text.to_string(),
+    }
+}
+
+// depending on charaters used, width may vary
+fn msg_to_btn_width(message: String) -> u8 {
+    let len = message.len();
+    let width = (len as f32 * 0.75).ceil() as u8 + UI_SCALE;
+    width.clamp(5, 200)
+}
+
+fn get_message_preview_btn(message: String) -> insim::insim::Btn {
+    insim::insim::Btn{
+        t: PREVIEW_OFFEST_TOP,
+        w: msg_to_btn_width(message.clone()),
+        h: UI_SCALE,
+        l: PREVIEW_OFFSET_LEFT,
+        reqi: insim::identifiers::RequestId::from(1),
+        ucid: insim::identifiers::ConnectionId::LOCAL,
+        clickid: insim::identifiers::ClickId::from(PREVIEW_ID),
+        inst: insim::insim::BtnInst::default(),
+        bstyle: insim::insim::BtnStyle{
+            colour: insim::insim::BtnStyleColour::NotEditable,
+            flags: insim::insim::BtnStyleFlags::LIGHT | insim::insim::BtnStyleFlags::LEFT,
+        },
+        typein: None,
+        caption: None,
+        text: format!("^3{}", message),
+    }
+}
+
